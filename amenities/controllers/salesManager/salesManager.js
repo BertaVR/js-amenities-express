9d04deb73f15bbe4598b63988342bb21e1b0670d
BertaVR <bverges@cifpfbmoll.eu>
2021-11-30 00:01:14 +0100

    fix: changed class name

--- amenities/controllers/salesManager/salesManager.js
+++ amenities/controllers/salesManager/salesManager.js
@@ -2,54 +2,53 @@ var store = require("../store/store");
 
 const myStore = store.singletonStore.getStore();
 
-function StoreManager() {
+function SalesManager() {
   //THIS CLASS IS FOR MANAGING ARRAYS!!
   this.store = myStore;
 }
 
-StoreManager.prototype.getStore = function () {
+SalesManager.prototype.getStore = function () {
   return this.store;
 };
 
-StoreManager.prototype.addPack = function (pack) {
+SalesManager.prototype.addPack = function (pack) {
   if (this.isAddableToStore(pack)) this.getStore().getInventory().add(pack);
 };
 
-StoreManager.prototype.filterByMaxPrice = function (maxPrice) {
+SalesManager.prototype.filterByMaxPrice = function (maxPrice) {
   return Array.from(this.getStore().getInventory()).filter(
     (p) => p.price <= maxPrice
   );
 };
 
-StoreManager.prototype.filterByMinPrice = function (minPrice) {
+SalesManager.prototype.filterByMinPrice = function (minPrice) {
   return Array.from(this.getStore().getInventory()).filter(
     (p) => p.price >= minPrice
   );
 };
 
-StoreManager.prototype.filterByContainsItem = function (minPrice) {
+SalesManager.prototype.filterByContainsItem = function (minPrice) {
   return Array.from(this.getStore().getInventory()).filter((p) =>
     p.items.includes(minPrice)
   );
 };
 
-StoreManager.prototype.filterByNumberOfItems = function (size) {
+SalesManager.prototype.filterByNumberOfItems = function (size) {
   return Array.from(this.getStore().getInventory()).filter(
     (p) => p.items.size() == size
   );
 };
 
 // CLOSURE
-StoreManager.prototype.filterTwoCriteria = function (filter1) {
+SalesManager.prototype.filterTwoCriteria = function (filter1) {
   let applyFirstFilter = filter1();
   return function filter(filter2) {
     filter2(applyFirstFilter);
   };
 };
 
-var factory = (function singleStoreManager() {
-  //I want my store manager o be singleton
-  const prototype = new StoreManager();
+var factory = (function singleSalesManager() {
+  const prototype = new SalesManager();
 
   return {
     getManager: function () {
