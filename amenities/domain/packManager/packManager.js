9cbd35128b7f85ad94cef3a7d6f75b229392f795
BertaVR <bverges@cifpfbmoll.eu>
2021-12-06 10:13:09 +0100

    feat: created class pack manager

---
+++ amenities/domain/packManager/packManager.js
@@ -0,0 +1,58 @@
+const packMaker = require("../pack");
+
+function PackManager() {
+  this.name = "My pack manager";
+}
+
+//El stock de un paquete será el del item que tenga menor stock: si ese item se acaba el paquete ya no se podrá vender
+
+PackManager.prototype.createStock = function (items) {
+  if (items == null || items == undefined) {
+    return 0;
+  }
+  const minStock = Math.min.apply(
+    Math,
+    Array.from(items).map((item) => item.stock)
+  );
+  return minStock;
+};
+
+// El precio es la suma del precio de los items + un porcentaje (de momento el 15, pero se podría cambiar en unas rebajas ya que va por parámetro)
+// La razón de que se reste un % es que cuando compras cosas por packs suele salir más barato
+PackManager.prototype.createPrecio = function (items) {
+  if (items == null || items == undefined) {
+    return 0;
+  }
+  return round(aplicarDescuento(sumItemsPrecio(items)));
+};
+
+function sumItemsPrecio(items) {
+  const sum = Array.from(items)
+    .map((item) => item.precio)
+    .reduce((a, b) => a + b);
+  return sum;
+}
+
+function aplicarDescuento(precio, porcentaje = 15) {
+  return precio * (1 - porcentaje / 100);
+}
+
+function round(num) {
+  return Math.round(num * 100) / 100;
+}
+
+var factory = (function singlePackManager() {
+  //I want my store manager o be singleton
+  const prototype = new PackManager();
+
+  return {
+    getManager: function () {
+      // SPRINT 3: CLOSURE
+      console.log("Creado pack manager");
+      return prototype;
+    },
+  };
+})();
+
+module.exports.packManager = factory;
+module.exports.functions = { aplicarDescuento, sumItemsPrecio, round };
