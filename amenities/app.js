77afbf17ad039beac7f2029c50fb941656511978
BertaVR <bverges@cifpfbmoll.eu>
2022-01-03 17:19:50 +0100

    draft: app

--- amenities/app.js
+++ amenities/app.js
@@ -5,8 +5,11 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const packsRouter = require("./routes/packs");
+const testRouter = require("./routes/test");
+
 
 var app = express();
 
@@ -25,6 +28,7 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 var express = require("express");
 const Pack = require("./models/Packs");
 
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -37,6 +41,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/packs", packsRouter);
+app.use("/test", testRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
