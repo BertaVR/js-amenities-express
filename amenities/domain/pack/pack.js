c1fdbf4d59cc506278bba2570f72a88bfa777703
BertaVR <bverges@cifpfbmoll.eu>
2022-01-05 00:20:30 +0100

    feat: added update pack to domain

--- amenities/domain/pack/pack.js
+++ amenities/domain/pack/pack.js
@@ -7,7 +7,6 @@ function Pack(nombre, items) {
   this.items = items; //array
   this.precio = myManager.createPrecio(items);
   this.calidad = myManager.createCalidad(items);
-
 }
 
 var factory = (() => {
@@ -17,6 +16,25 @@ var factory = (() => {
     },
   };
 })();
+
+Pack.prototype.updateItems = function (items) {
+  this.items = items;
+  this.stock = myManager.createStock(items);
+  this.precio = myManager.createPrecio(items);
+  this.calidad = myManager.createCalidad(items);
+};
+
+Pack.prototype.updatePack = function (field, value) {
+  switch (true) {
+    case field === "items":
+      this.updateItems(value);
+      break;
+    case field === "nombre":
+      this.nombre = value;
+      break;
+
+  }
+};
 
 Pack.prototype.getStock = function () {
   return this.stock;
@@ -40,4 +58,4 @@ isAvailable = function (pack) {
 
 module.exports.makePack = factory;
 module.exports.class = Pack;
-module.exports.functions = {isAvailable}
+module.exports.functions = { isAvailable };
