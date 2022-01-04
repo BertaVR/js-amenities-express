a01fa11dce30d0f522b97be05eb0a5f93626d330
BertaVR <bverges@cifpfbmoll.eu>
2022-01-04 15:34:31 +0100

    refactor: cambiado modelo

--- amenities/models/packs.js
+++ amenities/models/packs.js
@@ -8,23 +8,26 @@ const MATERIALES=  ['consumible', 'indestructible', 'normal'];
 var packSchema = new Schema({
     nombre: String,
     stock: Number,
-    items:  [{  nombre: String,
-        calidad :  Number,
-        material: String,
-        stock : Number, 
-        demanda : Number}],
-    precio:Number,
-    calidad: String,
-
+    items:  [{  nombre :{type: String, unique : true, required:true},
+        precio : {type: Number, required:true},
+        calidad : { type: Number, min: 0, max: 50 },
+        material : { type: String, enum: ["consumible", "indestructible", "normal"] },
+        stock : {type: Number, required:true}, 
+        demanda : { type: Number, min: 0, max: 100 }}],
+    precio: {type: Number, required:true}, 
+    calidad: { type: String, enum: ["basic", "standard", "premium"] },
 });
 
 packSchema.pre(['find', 'findOne'], function() {
-    // this instanceof mongoose.Query
-    // this se refiere a la query, no al documento, en este caso.
-    // Sirvo box sin la propiedad _v o version de documento 
-    // que genera monggose
+
     this.select('_id nombre items');
 
-    // next() es opcional
+  });
+
+
+  packSchema.pre(['findOneAndDelete'], function() {
+
+    this.select('_id nombre');
+
   });
 module.exports = mongoose.model("Packs", packSchema);
