841eb27312b1fbcbe674678c8cd45e20fc1531eb
BertaVR <bverges@cifpfbmoll.eu>
2022-01-04 10:51:37 +0100

    refactor: quitar código inútil

--- amenities/controllers/packsController.js
+++ amenities/controllers/packsController.js
@@ -11,40 +11,9 @@ var storeAPI = (function singleController() {
 
 
 
-    
-    // get a meeseeks
-    
-   const getInventory = function(req, res) {
-        // res.send('NOT IMPLEMENTED: Create meeseeks');
-        store.getInventory()
-        console.log("Number of items= ", inventory.size);
-
-        // destructuring
-        // Utilizo destructurig para forzar la busqueda en
-        // la cadena de prototipos del objeto MrMeeseeks {} 
-        // de sus propiedades message. Lo que hay en 
-        // reality es un objeto sin own properties 
-        // MrMeeseeks {} cuyo prototipo es el objeto
-        // que está en la propiedad this.mrMeeseeks
-        // de box
-        let {messageOnCreate: hi, messageOnRequest: greetings} = Array.from(inventory).pop();
-
-        // a la bbdd
-        let packInstance = new Pack(
-            {   nombre: "aaaa", 
-                messageOnRequest: greetings
-            }
-        );
-
-        inventory.save(function (err) {
-            if (err) return handleError(err);
-        });
-
-        res.status(200).type('json').json(packInstance);
-    }
 
     const getPack = ( (req, res, next) => {
-        console.log("ha llegado a la función");
+        console.log("ha llegado a la función del controlador");
 
         Packs.findOne({ 'nombre': `${req.params.nombre}` })
             .exec(function (err, pack) {
@@ -55,14 +24,13 @@ var storeAPI = (function singleController() {
       })
 
     const getAllPacks = ( (req, res, next) => {
-        console.log("ha llegado a la función");
+        console.log("ha llegado a la función del controlador");
 
         const packs =  Packs.find({}).exec(function (error, packs) {
             if (error) { return next(error); }
             // Successful, so render.
             res.status(200).type('json').json(packs);
         });
-          console.log(packs);
     })
     
     // public API
