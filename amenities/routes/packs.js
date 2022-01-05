552fbacfadb8cac83b0c39d6c915e464edeefcda
BertaVR <bverges@cifpfbmoll.eu>
2022-01-05 12:19:01 +0100

    feat: update de nombre ruta

--- amenities/routes/packs.js
+++ amenities/routes/packs.js
@@ -25,6 +25,8 @@ router.use(function (req, res, next) {
 router.get("/:nombre", packsController.storeAPI.getPack);
 router.get("/:nombre/delete", packsController.storeAPI.deletePack);
 router.post("/add",  packsController.storeAPI.createPack);
+router.get("/:nombre/cambiarNombre/:nuevoNombre", packsController.storeAPI.updateNombre);
+
 
 
 router.get("/", packsController.storeAPI.getAllPacks);
