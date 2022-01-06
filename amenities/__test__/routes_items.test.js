b233a894dcbfec7ae4a8bad2eba733699eb796e9
BertaVR <bverges@cifpfbmoll.eu>
2022-01-06 19:29:10 +0100

    test: getAllItems

--- amenities/__test__/routes_items.test.js
+++ amenities/__test__/routes_items.test.js
@@ -1,4 +1,5 @@
 const { expect } = require("@jest/globals");
+const { isValidObjectId } = require("mongoose");
 const request = require("supertest");
 const app = require("../app");
 
@@ -14,11 +15,10 @@ describe("Items Routes", () => {
       .then((res) => {
         expect(res.get("Content-Type")).toEqual(expect.stringMatching("/json"));
         expect(res.statusCode).toEqual(200);
-          expect(res.body.stock).toBe(63),
+        expect(res.body.stock).toBe(63),
           expect(res.body.demanda).toBe(63),
           expect(res.body.calidad).toBe(20),
           expect(res.body.precio).toBe(2),
-
           expect(res.body.nombre).toEqual(expect.stringMatching(nombre));
 
         expect(res.body._id).toBe("61d58b99d75d3770be596747");
@@ -33,4 +33,22 @@ describe("Items Routes", () => {
         expect(res.statusCode).toEqual(404);
       });
   });
-});
+
+  test("Positive test getAllItems /items /", () => {
+    return request(app)
+      .get(`/items`)
+      .then((res) => {
+        console.log(res.body);
+        // Received: "application/json; charset=utf-8"
+        expect(res.get("Content-Type")).toEqual(expect.stringMatching("/json"));
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveLength(71);
+        expect(res.body[0]._id).toBeTruthy();
+        expect(res.body[0].nombre).toBeTruthy();
+        expect(res.body[0].precio).toBeTruthy();
+        expect(res.body[0]).toHaveProperty("stock");
+        expect(res.body[0]).toHaveProperty("calidad");
+        expect(res.body[0]).toHaveProperty("demanda");
+      });
+  });
+}, 20000);
